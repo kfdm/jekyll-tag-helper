654b04d7e6436f251c02c6443ba9a2317d550937
Unknown <Unknown>
2019-01-03 10:11:06 +0900

    bundle gem jekyll-tag-helper

---
+++ lib/jekyll/tag/helper.rb
@@ -0,0 +1,10 @@
+require "jekyll/tag/helper/version"
+
+module Jekyll
+  module Tag
+    module Helper
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/tag/helper/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Tag
+    module Helper
+      VERSION = "0.1.0"
+    end
+  end
+end
